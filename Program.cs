2a43be3db984c4d41aa5267543cfc609a9472977
shintaaulya <shintaaulya20@gmail.com>
2025-03-23 21:52:08 +0700

    Implementasi design by contract

--- Program.cs
+++ Program.cs
@@ -4,7 +4,14 @@ class Program
 {
     static void Main()
     {
-        SayaTubeVideo video = new SayaTubeVideo("Tutorial Desain Berdasarkan Kontrak - Shinta Alya Aulya N");
-        video.CetakDetailVideo();
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - Shinta Alya Aulya N");
+
+        for (int i = 0; i < 5; i++)
+        {
+            video.IncreasePlayCount(2500000);
+        }
+
+        video.PrintVideoDetails();
     }
 }
+
--- SayaTubeVideo.cs
+++ SayaTubeVideo.cs
@@ -3,37 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace tpmodul6_103022300049
 {
     public class SayaTubeVideo
     {
         private int id;
-        private string judul;
-        private int jumlahPutar;
+        private string title;
+        private int playCount;
 
-        public SayaTubeVideo(string judul)
+        public SayaTubeVideo(string title)
         {
+            Debug.Assert(title != null, "Judul video tidak boleh null");
+            Debug.Assert(title.Length <= 100, "Judul video tidak boleh lebih dari 100 karakter");
+
             Random rand = new Random();
             this.id = rand.Next(10000, 99999);
-            this.judul = judul;
-            this.jumlahPutar = 0;
+            this.title = title;
+            this.playCount = 0;
         }
 
-        public void TambahJumlahPutar(int jumlah)
+        public void IncreasePlayCount(int count)
         {
-            jumlahPutar += jumlah;
+            Debug.Assert(count <= 10000000, "Maksimum play count per increment adalah 10.000.000");
+
+            try
+            {
+                checked
+                {
+                    playCount += count;
+                }
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Error: Play count melebihi batas maksimum integer!");
+            }
         }
 
-        public void CetakDetailVideo()
+        public void PrintVideoDetails()
         {
             Console.WriteLine($"ID: {id}");
-            Console.WriteLine($"Judul: {judul}");
-            Console.WriteLine($"Jumlah Putar: {jumlahPutar}");
+            Console.WriteLine($"Title: {title}");
+            Console.WriteLine($"Play Count: {playCount}");
         }
     }
 
 }
-
-
 
